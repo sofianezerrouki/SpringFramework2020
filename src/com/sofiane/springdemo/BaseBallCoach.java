6cca259b86951cac482108ccd42aaf9760dc21f9
sofiane <sofianeyooung@gmail.com>
2020-10-04 19:02:34 +0100

    add comment

--- src/com/sofiane/springdemo/BaseBallCoach.java
+++ src/com/sofiane/springdemo/BaseBallCoach.java
@@ -3,6 +3,6 @@ package com.sofiane.springdemo;
 public class BaseBallCoach implements Coach{
 	
 	public String getDailyWorkout() {
-		return "Spend 30 minute on batting practice ...";
+		return "Spend 30 minute on batting practice ... for me ";
 	}
 }
