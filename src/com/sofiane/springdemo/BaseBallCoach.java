f792203d6ec947360948eb28e574bd7307b201a2
sofiane <sofianeyooung@gmail.com>
2020-10-04 21:37:54 +0100

    last save

--- src/com/sofiane/springdemo/BaseBallCoach.java
+++ src/com/sofiane/springdemo/BaseBallCoach.java
@@ -3,6 +3,7 @@ package com.sofiane.springdemo;
 public class BaseBallCoach implements Coach{
 	
 	public String getDailyWorkout() {
-		return "Spend 30 minute on batting practice ... for me ";
+		return "Spend 30 minute on batting practice ... ";
 	}
+	
 }
