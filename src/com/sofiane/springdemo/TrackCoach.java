08f98407dfa0fe53e8747ef0ee9ff187fe0daab8
sofiane <sofianeyooung@gmail.com>
2020-10-07 12:39:04 +0100

    add a literal values injection ! like emailAdress ...

--- src/com/sofiane/springdemo/TrackCoach.java
+++ src/com/sofiane/springdemo/TrackCoach.java
@@ -4,6 +4,23 @@ public class TrackCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	//literal values
+	private String emailAddress;
+	public void setEmailAddress(String emailAddress) {
+		System.err.println("CricketCoach : inside setter methods : for setEmailAddress ///");
+		this.emailAddress = emailAddress;
+	}
+	public void setTeam(String team) {
+		System.err.println("CricketCoach : inside setter methods for setTeam ");
+		this.team = team;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public String getTeam() {
+		return team;
+	}
+	private String team;
 	
 	public TrackCoach(FortuneService fortuneService) {
 		super();
